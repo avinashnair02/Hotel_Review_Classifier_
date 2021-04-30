5948e45c23c53d1f39ded4e82cdad9e51a417581
Avinash Nair <avinashnair02@gmail.com>
2021-04-30 16:06:48 +0530

    changed the deployment file

--- Hilton_Hotel_App.py
+++ Hilton_Hotel_App.py
@@ -22,9 +22,9 @@ from keras.preprocessing import sequence
 
 #===============================================================================================#
 
-word_index_dict = pickle.load(open('C:/Users/Avinish/Desktop/scapy/nlp/nlp/spiders/Models//word_index_dict.pkl','rb'))
+word_index_dict = pickle.load(open('C:/Users/Avinish/Desktop/scapy/nlp/nlp/spiders/Models/word_index_dict.pkl','rb'))
 
-neural_net_model = load_model('C:/Users/Avinish/Desktop/scapy/nlp/nlp/spiders/Models/XGBoost.pkl')
+neural_net_model = load_model('C:/Users/Avinish/Desktop/scapy/nlp/nlp/spiders/Models/Neural_Network.h5')
 
 tokenizer = RegexpTokenizer(r'[a-zA-Z]+')
 
@@ -63,6 +63,22 @@ def text_cleanup(text):
 #===============================================================================================#
 
 sl.title("Hotel Review Classifier Application")
+page_bg_img = '''
+<style>
+body {
+background-image: url('https://images.unsplash.com/photo-1580835845971-a393b73bf370?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80');
+background-repeat: no-repeat;
+background-size: cover;
+}
+.block-container {
+    backdrop-filter: blur(0px);
+}
+.markdown-text-container > h1{text-align:center}
+</style>
+
+
+'''
+sl.markdown(page_bg_img, unsafe_allow_html=True)
 
 
 review_summary_text = sl.text_input('Enter Your Review Summary Here')
